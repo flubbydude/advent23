8c369c4713903bc084cfbc411fe990de585816e2
slustig210 <slustig@umass.edu>
2023-12-03 02:07:44 -0500

    +re day1

--- advent23_py/day1/solver.py
+++ advent23_py/day1/solver.py
@@ -1,13 +1,24 @@
-from aocd import get_data #type: ignore
+from aocd import get_data
 import re
 
 DAY = 1
 
 DIGIT_NUM_STRS = [str(i) for i in range(1, 10)]
-DIGIT_WORD_STRS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+DIGIT_WORD_STRS = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
 DIGIT_STRS = DIGIT_NUM_STRS + DIGIT_WORD_STRS
 
-DIGIT_STRS_RE = '|'.join(DIGIT_STRS)
+DIGIT_STRS_RE = "|".join(DIGIT_STRS)
+
 
 def part1(inp: list[str]):
     result = 0
@@ -22,36 +33,69 @@ def part1(inp: list[str]):
     return result
 
 
+def process_line(line: str):
+    first_appearances = (line.find(num_str) for num_str in DIGIT_STRS)
+    last_appearances = (line.rfind(num_str) for num_str in DIGIT_STRS)
+
+    _, first_index = min((j, i) for i, j in enumerate(first_appearances) if j != -1)
+    _, last_index = max((j, i) for i, j in enumerate(last_appearances) if j != -1)
+
+    if first_index >= 9:
+        first_index -= 9
+
+    if last_index >= 9:
+        last_index -= 9
+
+    return int(DIGIT_STRS[first_index] + DIGIT_STRS[last_index])
+
+
 def part2(inp: list[str]):
-    result = 0
+    return sum(process_line(line) for line in inp)
 
-    for line in inp:
-        first_appearances = (line.find(num_str) for num_str in DIGIT_STRS)
-        last_appearances = (line.rfind(num_str) for num_str in DIGIT_STRS)
 
-        _, first_index = min((j, i) for i, j in enumerate(first_appearances) if j != -1)
-        _, last_index = max((j, i) for i, j in enumerate(last_appearances) if j != -1)
+FIRST_DIGIT_STR_RE = rf".*?({DIGIT_STRS_RE})"
+REVERSED_FIRST_DIGIT_STR_RE = rf".*?({DIGIT_STRS_RE[::-1]})"
 
-        if first_index >= 9:
-            first_index -= 9
-        
-        if last_index >= 9:
-            last_index -= 9
 
-        result += int(DIGIT_STRS[first_index] + DIGIT_STRS[last_index])
+def process_line_re(line: str):
+    start_match = re.match(FIRST_DIGIT_STR_RE, line)
+    end_match = re.search(REVERSED_FIRST_DIGIT_STR_RE, line[::-1])
 
-    return result
+    assert start_match is not None
+    assert end_match is not None
+
+    first_digit_str = start_match.group(1)
+    last_digit_str = end_match.group(1)
+
+    first_index = DIGIT_STRS.index(first_digit_str)
+    last_index = DIGIT_STRS.index(last_digit_str[::-1])
+
+    if first_index >= 9:
+        first_index -= 9
+
+    if last_index >= 9:
+        last_index -= 9
+
+    return int(DIGIT_STRS[first_index] + DIGIT_STRS[last_index])
+
+
+def part2_re(inp: list[str]):
+    return sum(process_line_re(line) for line in inp)
+
 
 def parse_data(data: str):
     return data.splitlines()
+
 
 def main(data: str):
     inp = parse_data(data)
 
     print(part1(inp))
     print(part2(inp))
+    print(part2_re(inp))
 
-if __name__ == '__main__':
-    data = get_data(day=DAY, year=2023) #type: ignore
+
+if __name__ == "__main__":
+    data = get_data(day=DAY, year=2023)
     assert isinstance(data, str)
     main(data)
--- advent23_py/day1/solver_test.py
+++ advent23_py/day1/solver_test.py
@@ -1,5 +1,15 @@
 import unittest
-from solver import parse_data, part1, part2, DIGIT_NUM_STRS
+from .solver import (
+    parse_data,
+    part1,
+    part2,
+    part2_re,
+    DIGIT_NUM_STRS,
+    DAY,
+    process_line,
+    process_line_re,
+)
+from aocd import get_data
 
 PART1_TEST_DATA = """1abc2
 pqr3stu8vwx
@@ -16,19 +26,32 @@ zoneight234
 
 
 class Test(unittest.TestCase):
-
     def test_part1(self):
         part1_input = parse_data(PART1_TEST_DATA)
 
         self.assertEqual(part1(part1_input), 142)
 
     def test_digit_num_strs(self):
-        self.assertListEqual(DIGIT_NUM_STRS, ['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+        self.assertListEqual(
+            DIGIT_NUM_STRS, ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        )
 
     def test_part2(self):
         part2_input = parse_data(PART2_TEST_DATA)
 
         self.assertEqual(part2(part2_input), 281)
 
-if __name__ == '__main__':
+    def test_part2_re(self):
+        part2_input = parse_data(PART2_TEST_DATA)
+
+        self.assertEqual(part2_re(part2_input), 281)
+
+    def test_part2_re_comparitively(self):
+        part2_input = parse_data(get_data(day=DAY, year=2023))
+
+        for line in part2_input:
+            self.assertEqual(process_line(line), process_line_re(line), f"{line = }")
+
+
+if __name__ == "__main__":
     unittest.main()
