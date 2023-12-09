3450dfc1ad1aa66932cb109c22dd16bc2a2ca566
slustig210 <slustig@umass.edu>
2023-12-08 22:02:57 -0500

    part 1 and some more like djikstra's

--- advent23_rs/day8/src/main.rs
+++ advent23_rs/day8/src/main.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use priority_queue::PriorityQueue;
+use regex::Regex;
 use std::collections::HashMap;
 
 enum Direction {
@@ -5,24 +8,88 @@ enum Direction {
     Right,
 }
 
-type Graph<'a> = HashMap<&'a str, (&'a str, &'a str)>;
+type Graph<'a> = HashMap<&'a str, [&'a str; 2]>;
 
-fn part1(instructions: &[Direction], graph: &Graph) -> i32 {
+fn part1(instructions: &[Direction], graph: &Graph) -> usize {
     // follow instructions repeatedly on graph until you get to ZZZ
     // return the number of instructions followed
-    todo!()
+    let mut current = "AAA";
+    for (i, instruction) in instructions.iter().cycle().enumerate() {
+        if current == "ZZZ" {
+            return i;
+        }
+
+        current = graph[current][*instruction as usize];
+    }
+
+    unreachable!()
+}
+
+trait GraphExt {
+    fn djikstras(&self, start: &str) -> HashMap<&str, i32>;
+    fn all_source_djikstras(&self) -> HashMap<&str, HashMap<&str, i32>>;
+}
+
+impl GraphExt for Graph<'_> {
+    fn djikstras(&self, start: &str) -> HashMap<&str, i32> {
+        let mut queue = PriorityQueue::new();
+        let mut dists = HashMap::new();
+        for &node in self.keys() {
+            dists.insert(node, if node == start { 0 } else { i32::MAX });
+            queue.push(node, if node == start { 0 } else { i32::MAX });
+        }
+
+        while !queue.is_empty() {
+            let (node, _) = queue.pop().unwrap();
+
+            for &neighbor in self.get(node).unwrap() {
+                let alt = dists[node] + 1;
+                if alt < dists[neighbor] {
+                    dists[neighbor] = alt;
+                    queue.push_decrease(neighbor, alt);
+                }
+            }
+        }
+
+        dists
+    }
+
+    fn all_source_djikstras(&self) -> HashMap<&str, HashMap<&str, i32>> {
+        self.keys()
+            .map(|&node| (node, self.djikstras(node)))
+            .collect()
+    }
+}
+
+fn part2(graph: &Graph) -> i32 {
+    let ends_in_a = graph
+        .keys()
+        .filter(|&node| node.ends_with('a'))
+        .cloned()
+        .collect::<Vec<_>>();
+
+    let ends_in_z = graph
+        .keys()
+        .filter(|&node| node.ends_with('z'))
+        .cloned()
+        .collect::<Vec<_>>();
 }
 
 fn parse_input(puzzle_input: &str) -> (Vec<Direction>, Graph) {
     let mut iter = puzzle_input.lines();
-    let instructions = iter.next().unwrap().chars().map(|c| match c {
-        'L' => Direction::Left,
-        'R' => Direction::Right,
-        _ => panic!(
-            "Instructions characters must be 'L' or 'R', received: {}",
-            c
-        ),
-    });
+    let instructions = iter
+        .next()
+        .unwrap()
+        .chars()
+        .map(|c| match c {
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            _ => panic!(
+                "Instructions characters must be 'L' or 'R', received: {}",
+                c
+            ),
+        })
+        .collect();
 
     // empty line
     iter.next();
@@ -30,8 +97,13 @@ fn parse_input(puzzle_input: &str) -> (Vec<Direction>, Graph) {
     let mut graph = HashMap::new();
     for line in iter {
         // ex line: "NQT = (TXC, RVJ)"
+        lazy_static! {
+            static ref LINE_REGEX: Regex = Regex::new(r#"(\w+) = \((\w+), (\w+)\)"#).unwrap();
+        }
 
-        // graph.insert(, v)
+        let (_, [node, neighbor1, neighbor2]) = LINE_REGEX.captures(line).unwrap().extract();
+
+        graph.insert(node, [neighbor1, neighbor2]);
     }
 
     (instructions, graph)
@@ -41,8 +113,10 @@ fn main() {
     let file_contents = std::fs::read("input.txt").unwrap();
     let puzzle_input = std::str::from_utf8(&file_contents).unwrap();
 
-    println!("{}", part1(puzzle_input));
-    // println!("{}", part2(puzzle_input));
+    let (instructions, graph) = parse_input(puzzle_input);
+
+    println!("{}", part1(&instructions, &graph));
+    println!("{}", part2(&graph));
 }
 
 #[cfg(test)]
@@ -65,12 +139,16 @@ mod tests {
 
     #[test]
     fn test_part1_a() {
-        assert_eq!(part1(TEST_INPUT_A), 2);
+        let (instructions, graph) = parse_input(TEST_INPUT_A);
+
+        assert_eq!(part1(&instructions, &graph), 2);
     }
 
     #[test]
     fn test_part1_b() {
-        assert_eq!(part1(TEST_INPUT_B), 6);
+        let (instructions, graph) = parse_input(TEST_INPUT_B);
+
+        assert_eq!(part1(&instructions, &graph), 6);
     }
 
     // #[test]
