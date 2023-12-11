b1be337df24b368439195598e84b06893abd1565
flubbydude <stueyrama@gmail.com>
2023-12-10 20:13:09 -0500

    move to lab tob for debug

--- advent23_rs/day10/src/main.rs
+++ advent23_rs/day10/src/main.rs
@@ -1,4 +1,8 @@
-use std::mem;
+use std::{
+    fs::File,
+    io::{self, Write},
+    mem,
+};
 
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
@@ -14,22 +18,22 @@ enum Direction {
     West,
 }
 
-#[derive(FromPrimitive, Clone, Copy)]
+#[derive(Debug, FromPrimitive, Clone, Copy)]
 enum Turn {
-    Right,
-    Left,
     Straight,
+    Right,
     Backwards,
+    Left,
 }
 
 impl Direction {
-    fn get_successor(&self, i: usize, j: usize) -> (usize, usize) {
+    fn get_successor(&self, i: usize, j: usize) -> (isize, isize) {
         use Direction::*;
         match *self {
-            North => (i - 1, j),
-            South => (i + 1, j),
-            East => (i, j + 1),
-            West => (i, j - 1),
+            North => (i as isize - 1, j as isize),
+            South => (i as isize + 1, j as isize),
+            East => (i as isize, j as isize + 1),
+            West => (i as isize, j as isize - 1),
         }
     }
 
@@ -99,10 +103,10 @@ impl Tile {
 enum TileInfo {
     Enclosed,
     InLoop,
-    NotEnclosed,
+    // NotEnclosed,
 }
 
-fn find_s_and_start_dir(puzzle_input: &[&[Tile]], num_cols: usize) -> (usize, usize, Direction) {
+fn get_intitial_pipe_state(puzzle_input: &[&[Tile]], num_cols: usize) -> PipeState {
     let (s_row, s_col) = puzzle_input
         .iter()
         .enumerate()
@@ -110,70 +114,124 @@ fn find_s_and_start_dir(puzzle_input: &[&[Tile]], num_cols: usize) -> (usize, us
         .next()
         .unwrap();
 
-    let start_dir = Direction::iter()
-        .filter_map(|dir| {
+    Direction::iter()
+        .find_map(|dir| {
             let (start_row, start_col) = dir.get_successor(s_row, s_col);
 
-            if start_row < puzzle_input.len()
+            if start_row < 0 || start_col < 0 {
+                return None;
+            }
+
+            let start_row = start_row as usize;
+            let start_col = start_col as usize;
+
+            if (start_row < puzzle_input.len()
                 && start_col < num_cols
-                && puzzle_input[start_row][start_col].connects_to(dir.opposite())
+                && puzzle_input[start_row][start_col].connects_to(dir.opposite()))
             {
-                Some(dir)
+                Some(PipeState {
+                    row: start_row,
+                    col: start_col,
+                    dir,
+                })
             } else {
                 None
             }
         })
-        .next()
-        .unwrap();
+        .unwrap()
+}
 
-    (s_row, s_col, start_dir)
+struct PipeIterator<'a> {
+    puzzle_input: &'a [&'a [Tile]],
+    num_cols: usize,
+    state: PipeState,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct PipeState {
+    row: usize,
+    col: usize,
+    dir: Direction,
+}
+
+fn get_pipe_iterator<'a>(
+    puzzle_input: &'a [&'a [Tile]],
+    num_cols: usize,
+    initial_state: PipeState,
+) -> PipeIterator<'a> {
+    PipeIterator {
+        puzzle_input,
+        num_cols,
+        state: initial_state,
+    }
+}
+
+impl Iterator for PipeIterator<'_> {
+    type Item = PipeState;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let state = &mut self.state;
+
+        if self.puzzle_input[state.row][state.col] == Tile::Start {
+            return None;
+        }
+
+        // get next direction
+        let [d1, d2] = self.puzzle_input[state.row][state.col].dirs().unwrap();
+
+        state.dir = if d1 == state.dir.opposite() { d2 } else { d1 };
+
+        let (next_row, next_col) = state.dir.get_successor(state.row, state.col);
+
+        assert!(next_row >= 0 && next_col >= 0);
+
+        state.row = next_row as usize;
+        state.col = next_col as usize;
+
+        assert!(state.row < self.puzzle_input.len());
+        assert!(state.col < self.num_cols);
+
+        Some(*state)
+    }
 }
 
 fn part1(puzzle_input: &[&[Tile]], num_cols: usize) -> usize {
     // first find the position of the S
-    let (s_row, s_col, start_dir) = find_s_and_start_dir(puzzle_input, num_cols);
+    // go 1 step in a direction
+    // follow the pipe until reaching S
+    // count the number of time taken to reach S
+    // divide by 2, + 1 for prob cuz 0 is double counted
+    let num_steps = get_pipe_iterator(
+        puzzle_input,
+        num_cols,
+        get_intitial_pipe_state(puzzle_input, num_cols),
+    )
+    .count();
 
-    let (mut row, mut col) = start_dir.get_successor(s_row, s_col);
-    let mut prev_dir = start_dir;
-    for i in 1.. {
-        if row == s_row && col == s_col {
-            return i / 2;
-        }
-
-        let dirs = puzzle_input[row][col].dirs().unwrap();
-
-        let dir = if dirs[0] == prev_dir.opposite() {
-            dirs[1]
-        } else {
-            dirs[0]
-        };
-
-        (row, col) = dir.get_successor(row, col);
-        prev_dir = dir;
-    }
-
-    unreachable!()
+    (num_steps + 1) / 2
 }
 
-// assume the flood fill never reachest the edges of the array (0 or num rows or num cols)
-fn flood_fill(
-    tile_info_arr: &mut [Vec<Option<TileInfo>>],
-    i: usize,
-    j: usize,
-    prev_dir: Option<Direction>,
-) {
-    if tile_info_arr[i][j].is_some() {
-        return;
-    }
-
+fn flood_fill(tile_info_arr: &mut [Vec<Option<TileInfo>>], num_cols: usize, i: usize, j: usize) {
     tile_info_arr[i][j] = Some(TileInfo::Enclosed);
 
     for dir in Direction::iter() {
-        if prev_dir.is_some() && dir == prev_dir.unwrap().opposite() {
+        let (new_i, new_j) = dir.get_successor(i, j);
+        if new_i < 0 || new_j < 0 {
             continue;
         }
-        let (new_i, new_j) = dir.get_successor(i, j);
-        flood_fill(tile_info_arr, new_i, new_j, Some(dir));
+
+        let new_i = new_i as usize;
+        let new_j = new_j as usize;
+
+        if new_i >= tile_info_arr.len() || new_j >= num_cols {
+            continue;
+        }
+
+        if tile_info_arr[new_i][new_j].is_some() {
+            continue;
+        }
+
+        flood_fill(tile_info_arr, num_cols, new_i, new_j);
     }
 }
 
@@ -188,36 +246,24 @@ fn part2(puzzle_input: &[&[Tile]], num_cols: usize) -> usize {
         tile_info_arr.push(v);
     }
 
-    let (s_row, s_col, start_dir) = find_s_and_start_dir(puzzle_input, num_cols);
-    let (mut row, mut col) = start_dir.get_successor(s_row, s_col);
-    let mut prev_dir = start_dir;
-
     // follow main loop and count lefts vs rights
+    let initial_state = get_intitial_pipe_state(puzzle_input, num_cols);
+
+    let mut prev_dir = initial_state.dir;
+
     let mut num_lefts = 0;
     let mut num_rights = 0;
-    loop {
-        tile_info_arr[row][col] = Some(TileInfo::InLoop);
 
-        if row == s_row && col == s_col {
-            break;
-        }
-
-        let dirs = puzzle_input[row][col].dirs().unwrap();
-
-        let dir = if dirs[0] == prev_dir.opposite() {
-            dirs[1]
-        } else {
-            dirs[0]
-        };
-
-        match dir.get_turn_since_last(prev_dir) {
+    for pipe_state in get_pipe_iterator(puzzle_input, num_cols, initial_state) {
+        match pipe_state.dir.get_turn_since_last(prev_dir) {
             Turn::Right => num_rights += 1,
             Turn::Left => num_lefts += 1,
             _ => (),
         }
 
-        (row, col) = dir.get_successor(row, col);
-        prev_dir = dir;
+        tile_info_arr[pipe_state.row][pipe_state.col] = Some(TileInfo::InLoop);
+
+        prev_dir = pipe_state.dir;
     }
 
     let inside_turn = if num_rights > num_lefts {
@@ -226,53 +272,91 @@ fn part2(puzzle_input: &[&[Tile]], num_cols: usize) -> usize {
         Turn::Left
     };
 
-    let (mut row, mut col) = start_dir.get_successor(s_row, s_col);
-    let mut prev_dir = start_dir;
+    println!("num rights = {num_rights}, num lefts = {num_lefts} => {inside_turn:?} is inside");
 
     // follow main loop again with same starts, but this time
     // turn inside_turn every step and set it in the array
     // tile_info_arr
-    loop {
-        let (in_row, in_col) = prev_dir.turn(inside_turn).get_successor(row, col);
-        if tile_info_arr[in_row][in_col].is_none() {
-            tile_info_arr[in_row][in_col] = Some(TileInfo::Enclosed);
+    prev_dir = initial_state.dir;
+    for pipe_state in get_pipe_iterator(puzzle_input, num_cols, initial_state) {
+        // (irow, icol) is on the inside of the loop if not on the loop
+        let (irow, icol) = prev_dir
+            .turn(inside_turn)
+            .get_successor(pipe_state.row, pipe_state.col);
+
+        assert!(irow >= 0 && icol >= 0);
+        let irow = irow as usize;
+        let icol = icol as usize;
+        assert!(irow < puzzle_input.len() && icol < num_cols);
+
+        if tile_info_arr[irow][icol].is_none() {
+            tile_info_arr[irow][icol] = Some(TileInfo::Enclosed);
         }
 
-        let dirs = puzzle_input[row][col].dirs().unwrap();
+        prev_dir = pipe_state.dir;
 
-        let dir = if dirs[0] == prev_dir.opposite() {
-            dirs[1]
-        } else {
-            dirs[0]
-        };
+        // go againe
+        let (irow, icol) = prev_dir
+            .turn(inside_turn)
+            .get_successor(pipe_state.row, pipe_state.col);
 
-        let (in_row, in_col) = dir.turn(inside_turn).get_successor(row, col);
-        if tile_info_arr[in_row][in_col].is_none() {
-            tile_info_arr[in_row][in_col] = Some(TileInfo::Enclosed);
+        assert!(irow >= 0 && icol >= 0);
+        let irow = irow as usize;
+        let icol = icol as usize;
+        assert!(irow < puzzle_input.len() && icol < num_cols);
+
+        if tile_info_arr[irow][icol].is_none() {
+            tile_info_arr[irow][icol] = Some(TileInfo::Enclosed);
         }
-
-        if row == s_row && col == s_col {
-            break;
-        }
-
-        (row, col) = dir.get_successor(row, col);
-        prev_dir = dir;
     }
+
+    print_tile_info_and_main_loop("main_loop.txt", puzzle_input, &tile_info_arr, true).unwrap();
+
+    print_tile_info_and_main_loop("out1.txt", puzzle_input, &tile_info_arr, false).unwrap();
 
     for i in 0..tile_info_arr.len() {
         for j in 0..num_cols {
-            flood_fill(&mut tile_info_arr, i, j, None);
+            if matches!(tile_info_arr[i][j], Some(TileInfo::Enclosed)) {
+                flood_fill(&mut tile_info_arr, num_cols, i, j);
+            }
         }
     }
+
+    print_tile_info_and_main_loop("out2.txt", puzzle_input, &tile_info_arr, false).unwrap();
 
     tile_info_arr
         .iter()
         .flat_map(|row| row.iter())
-        .filter(|cell| match cell {
-            Some(TileInfo::Enclosed) => true,
-            _ => false,
-        })
+        .filter(|cell| matches!(cell, Some(TileInfo::Enclosed)))
         .count()
+}
+
+fn print_tile_info_and_main_loop(
+    output_filename: &str,
+    puzzle_input: &[&[Tile]],
+    tile_info_arr: &[Vec<Option<TileInfo>>],
+    print_main_loop_only: bool,
+) -> Result<(), io::Error> {
+    let mut file = File::create(output_filename)?;
+    for (tile_row, info_row) in puzzle_input.iter().zip(tile_info_arr.iter()) {
+        for (tile, row) in tile_row.iter().zip(info_row.iter()) {
+            let c_to_write = match row {
+                Some(TileInfo::InLoop) => *tile as u8,
+                Some(TileInfo::Enclosed) => {
+                    if print_main_loop_only {
+                        b'.'
+                    } else {
+                        b'I'
+                    }
+                }
+                None => b'.',
+            };
+            write!(file, "{}", c_to_write as char)?;
+        }
+        writeln!(file)?;
+    }
+
+    Ok(())
 }
 
 fn main() {
@@ -293,4 +377,41 @@ fn main() {
 
     println!("{}", part1(&puzzle_input, num_cols));
     println!("{}", part2(&puzzle_input, num_cols));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT_A: &str = ".F----7F7F7F7F-7....\n\
+                                .|F--7||||||||FJ....\n\
+                                .||.FJ||||||||L7....\n\
+                                FJL7L7LJLJ||LJ.L-7..\n\
+                                L--J.L7...LJS7F-7L7.\n\
+                                ....F-J..F7FJ|L7L7L7\n\
+                                ....L7.F7||L7|.L7L7|\n\
+                                .....|FJLJ|FJ|F7|.LJ\n\
+                                ....FJL-7.||.||||...\n\
+                                ....L---J.LJ.LJLJ...";
+
+    fn test_part_2(input_as_str: &str, answer: usize) {
+        let puzzle_input: Vec<&[Tile]>;
+
+        // assuming input is valid:
+        unsafe {
+            puzzle_input = input_as_str
+                .lines()
+                .map(|line| mem::transmute(line.as_bytes()))
+                .collect();
+        }
+
+        let num_cols = puzzle_input[0].len();
+
+        assert_eq!(part2(&puzzle_input, num_cols), answer);
+    }
+
+    #[test]
+    fn stu_test() {
+        test_part_2(TEST_INPUT_A, 8);
+    }
 }
