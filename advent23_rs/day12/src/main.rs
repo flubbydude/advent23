382d6ea5a23c36a10b4f0cd436b63345ad637f5f
flubbydude <stueyrama@gmail.com>
2023-12-13 00:43:04 -0500

    worky but mucho slow

--- advent23_rs/day12/src/main.rs
+++ advent23_rs/day12/src/main.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 #[derive(Clone)]
 struct RecordRow {
     damaged_record: Vec<u8>,
-    num_contiguous: Vec<usize>,
+    num_contiguous: Vec<u32>,
 }
 
 impl From<&str> for RecordRow {
@@ -26,11 +28,11 @@ impl From<&str> for RecordRow {
 }
 
 impl RecordRow {
-    fn num_ways_helper(
+    fn _num_ways_recursive_helper(
         &self,
         record_index: usize,
         contiguous_index: usize,
-        prev_damaged: usize,
+        prev_damaged: u32,
     ) -> usize {
         if record_index == self.damaged_record.len() {
             let is_valid = if contiguous_index == self.num_contiguous.len() {
@@ -49,36 +51,37 @@ impl RecordRow {
             {
                 0
             } else {
-                self.num_ways_helper(record_index + 1, contiguous_index, prev_damaged + 1)
+                self._num_ways_recursive_helper(
+                    record_index + 1,
+                    contiguous_index,
+                    prev_damaged + 1,
+                )
             }
         };
 
-        let handle_empty = || {
+        let handle_operational = || {
             if prev_damaged == 0 {
-                self.num_ways_helper(record_index + 1, contiguous_index, 0)
+                self._num_ways_recursive_helper(record_index + 1, contiguous_index, 0)
             } else if contiguous_index == self.num_contiguous.len()
                 || self.num_contiguous[contiguous_index] != prev_damaged
             {
                 0
             } else {
                 // move 1 forward in the contiguous index
-                self.num_ways_helper(record_index + 1, contiguous_index + 1, 0)
+                self._num_ways_recursive_helper(record_index + 1, contiguous_index + 1, 0)
             }
         };
 
         match self.damaged_record[record_index] {
             b'#' => handle_damaged(),
-            b'.' => handle_empty(),
-            b'?' => handle_damaged() + handle_empty(),
+            b'.' => handle_operational(),
+            b'?' => handle_damaged() + handle_operational(),
             _ => panic!("Input contains an unexpected character."),
         }
     }
 
-    fn num_ways(&self) -> usize {
-        // greedy recursive time :(
-        // prev_damaged is the number I have seen in a row that are damaged
-        // if at the end of the record
-        self.num_ways_helper(0, 0, 0)
+    fn _num_ways_recursive(&self) -> usize {
+        self._num_ways_recursive_helper(0, 0, 0)
     }
 
     fn unfolded(&self) -> Self {
@@ -97,6 +100,94 @@ impl RecordRow {
         }
 
         result
+    }
+
+    fn num_ways(&self) -> usize {
+        let mut num_contiguous_end_damaged: HashMap<Vec<u32>, usize> = HashMap::new();
+        let mut num_contiguous_end_operational: HashMap<Vec<u32>, usize> =
+            HashMap::from([(vec![], 1)]);
+
+        let handle_damaged =
+            |prev_damaged: HashMap<Vec<u32>, usize>, prev_operational: HashMap<Vec<u32>, usize>| {
+                let mut result = prev_damaged
+                    .into_iter()
+                    .filter_map(|(mut comb_end_damaged, num_ways)| {
+                        // should always be true that this vec is not empty!
+                        let i = comb_end_damaged.len() - 1;
+                        let last_elem = comb_end_damaged.last_mut().unwrap();
+
+                        *last_elem += 1;
+
+                        // new combinations is too big!
+                        if *last_elem > self.num_contiguous[i] {
+                            None
+                        } else {
+                            Some((comb_end_damaged, num_ways))
+                        }
+                    })
+                    .collect::<HashMap<_, _>>();
+
+                for (mut comb_end_operational, num_ways) in prev_operational {
+                    // skip this element; can't add another contiguous to it bc it would be too many separate groups.
+                    if comb_end_operational.len() == self.num_contiguous.len() {
+                        continue;
+                    }
+
+                    comb_end_operational.push(1);
+
+                    result
+                        .entry(comb_end_operational)
+                        .and_modify(|val| *val += num_ways)
+                        .or_insert(num_ways);
+                }
+
+                result
+            };
+
+        for c in self.damaged_record.iter() {
+            match c {
+                b'#' => {
+                    num_contiguous_end_damaged =
+                        handle_damaged(num_contiguous_end_damaged, num_contiguous_end_operational);
+
+                    num_contiguous_end_operational = HashMap::new();
+                }
+                b'.' => {
+                    for (comb_end_damaged, num_ways) in num_contiguous_end_damaged {
+                        num_contiguous_end_operational
+                            .entry(comb_end_damaged)
+                            .and_modify(|val| *val += num_ways)
+                            .or_insert(num_ways);
+                    }
+
+                    num_contiguous_end_damaged = HashMap::new();
+                }
+                b'?' => {
+                    let mut new_operational = num_contiguous_end_operational.clone();
+
+                    for (comb_end_damaged, &num_ways) in num_contiguous_end_damaged.iter() {
+                        if let Some(val) = new_operational.get_mut(comb_end_damaged) {
+                            *val += num_ways;
+                        } else {
+                            new_operational.insert(comb_end_damaged.clone(), num_ways);
+                        }
+                    }
+
+                    num_contiguous_end_damaged =
+                        handle_damaged(num_contiguous_end_damaged, num_contiguous_end_operational);
+
+                    num_contiguous_end_operational = new_operational;
+                }
+                _ => panic!(),
+            }
+        }
+
+        num_contiguous_end_damaged
+            .get(&self.num_contiguous)
+            .unwrap_or(&0)
+            + num_contiguous_end_operational
+                .get(&self.num_contiguous)
+                .unwrap_or(&0)
     }
 }
 
