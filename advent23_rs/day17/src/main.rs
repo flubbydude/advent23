d298f9d08451afcd1a1852e9702c9f4d4258aed8
flubbydude <stueyrama@gmail.com>
2023-12-18 01:48:55 -0500

    +day17 part 2

--- advent23_rs/day17/src/main.rs
+++ advent23_rs/day17/src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use array2d::Array2D;
 
 mod search;
@@ -62,7 +61,7 @@ impl Position {
         Position { row, column }
     }
 
-    fn _manhattan_distance(&self, other: &Self) -> usize {
+    fn manhattan_distance(&self, other: &Self) -> usize {
         let di = if self.row > other.row {
             self.row - other.row
         } else {
@@ -128,11 +127,21 @@ struct State {
 }
 
 impl State {
-    fn get_successors(&self, puzzle_input: &Array2D<u8>) -> Box<[(usize, State)]> {
+    fn get_successors(
+        &self,
+        puzzle_input: &Array2D<u8>,
+        is_ultra_crucible: bool,
+    ) -> Vec<(usize, State)> {
         let mut result = Vec::new();
 
+        let can_go_straight = if is_ultra_crucible {
+            self.num_straight < 10
+        } else {
+            self.num_straight < 3
+        };
+
         // try going straight
-        if self.num_straight != 2 {
+        if can_go_straight {
             if let Some(position) = self.position.move_in_dir(
                 self.direction,
                 puzzle_input.num_rows(),
@@ -149,45 +158,54 @@ impl State {
             }
         }
 
-        // try turning right
-        let right = self.direction.turn_right();
+        let can_turn = if is_ultra_crucible {
+            self.num_straight >= 4
+        } else {
+            true
+        };
 
-        if let Some(position) =
-            self.position
-                .move_in_dir(right, puzzle_input.num_rows(), puzzle_input.num_columns())
-        {
-            result.push((
-                puzzle_input[(position.row, position.column)] as usize,
-                State {
-                    position,
-                    direction: right,
-                    num_straight: 0,
-                },
-            ))
+        if can_turn {
+            // try turning right
+            let right = self.direction.turn_right();
+
+            if let Some(position) = self.position.move_in_dir(
+                right,
+                puzzle_input.num_rows(),
+                puzzle_input.num_columns(),
+            ) {
+                result.push((
+                    puzzle_input[(position.row, position.column)] as usize,
+                    State {
+                        position,
+                        direction: right,
+                        num_straight: 1,
+                    },
+                ))
+            }
+
+            // try turning left
+            let left = self.direction.turn_left();
+
+            if let Some(position) =
+                self.position
+                    .move_in_dir(left, puzzle_input.num_rows(), puzzle_input.num_columns())
+            {
+                result.push((
+                    puzzle_input[(position.row, position.column)] as usize,
+                    State {
+                        position,
+                        direction: left,
+                        num_straight: 1,
+                    },
+                ))
+            }
         }
 
-        // try turning left
-        let left = self.direction.turn_left();
-
-        if let Some(position) =
-            self.position
-                .move_in_dir(left, puzzle_input.num_rows(), puzzle_input.num_columns())
-        {
-            result.push((
-                puzzle_input[(position.row, position.column)] as usize,
-                State {
-                    position,
-                    direction: left,
-                    num_straight: 0,
-                },
-            ))
-        }
-
-        result.into_boxed_slice()
+        result
     }
 }
 
-fn part1(puzzle_input: &Array2D<u8>) -> usize {
+fn get_minimum_heat(puzzle_input: &Array2D<u8>, is_ultra_crucible: bool) -> usize {
     let initial_states = [
         State {
             position: Position::ZERO,
@@ -203,27 +221,31 @@ fn part1(puzzle_input: &Array2D<u8>) -> usize {
 
     let goal_position = Position::new(puzzle_input.num_rows() - 1, puzzle_input.num_columns() - 1);
 
-    let (total_cost, _path) = a_star_search(
+    // ultra crucible must go straight 4 times before goal.
+    a_star_search(
         &initial_states,
-        |state| state.get_successors(puzzle_input),
-        |state| state.position == goal_position,
-        |state| state.position._manhattan_distance(&goal_position),
+        |state| state.get_successors(puzzle_input, is_ultra_crucible),
+        |state| state.position == goal_position && (!is_ultra_crucible || state.num_straight >= 4),
+        |state| state.position.manhattan_distance(&goal_position),
     )
-    .unwrap();
-
-    // total cost = total heat loss
-    total_cost
+    .unwrap()
 }
 
-fn main() -> Result<()> {
-    let file_contents = std::fs::read_to_string("input.txt")?;
+fn part1(puzzle_input: &Array2D<u8>) -> usize {
+    get_minimum_heat(puzzle_input, false)
+}
+
+fn part2(puzzle_input: &Array2D<u8>) -> usize {
+    get_minimum_heat(puzzle_input, true)
+}
+
+fn main() {
+    let file_contents = std::fs::read_to_string("input.txt").unwrap();
 
     let puzzle_input = parse_input(&file_contents);
 
     println!("{}", part1(&puzzle_input));
-    // println!("{}", part2(&puzzle_input));
-
-    Ok(())
+    println!("{}", part2(&puzzle_input));
 }
 
 #[cfg(test)]
@@ -231,26 +253,44 @@ mod tests {
 
     use super::*;
 
-    const TEST_INPUT: &str = "2413432311323\n\
-                              3215453535623\n\
-                              3255245654254\n\
-                              3446585845452\n\
-                              4546657867536\n\
-                              1438598798454\n\
-                              4457876987766\n\
-                              3637877979653\n\
-                              4654967986887\n\
-                              4564679986453\n\
-                              1224686865563\n\
-                              2546548887735\n\
-                              4322674655533";
+    const TEST_INPUT_A: &str = "2413432311323\n\
+                                3215453535623\n\
+                                3255245654254\n\
+                                3446585845452\n\
+                                4546657867536\n\
+                                1438598798454\n\
+                                4457876987766\n\
+                                3637877979653\n\
+                                4654967986887\n\
+                                4564679986453\n\
+                                1224686865563\n\
+                                2546548887735\n\
+                                4322674655533";
+
+    const TEST_INPUT_B: &str = "111111111111\n\
+                                999999999991\n\
+                                999999999991\n\
+                                999999999991\n\
+                                999999999991";
 
     #[test]
-    fn test_part_1() -> Result<()> {
-        let puzzle_input = parse_input(TEST_INPUT);
+    fn test_part_1() {
+        let puzzle_input = parse_input(TEST_INPUT_A);
 
         assert_eq!(part1(&puzzle_input), 102);
+    }
 
-        Ok(())
+    #[test]
+    fn test_part_2a() {
+        let puzzle_input = parse_input(TEST_INPUT_A);
+
+        assert_eq!(part2(&puzzle_input), 94);
+    }
+
+    #[test]
+    fn test_part_2b() {
+        let puzzle_input = parse_input(TEST_INPUT_B);
+
+        assert_eq!(part2(&puzzle_input), 71);
     }
 }
--- advent23_rs/day17/src/search.rs
+++ advent23_rs/day17/src/search.rs
@@ -2,17 +2,83 @@ use num::PrimInt;
 use priority_queue::PriorityQueue;
 use std::{cmp::Reverse, collections::HashMap, hash::Hash};
 
+// get the cost and the path taken
+pub fn a_star_search<T, C>(
+    initial_states: &[T],
+
+    // boxed slice of (cost to get to successor, successor)
+    get_successors: impl Fn(&T) -> Vec<(C, T)>,
+
+    is_goal: impl Fn(&T) -> bool,
+    heuristic: impl Fn(&T) -> C,
+) -> Option<C>
+where
+    T: Clone + PartialEq + Eq + Hash,
+    C: PrimInt,
+{
+    let mut frontier = initial_states
+        .iter()
+        .cloned()
+        .map(|state| {
+            let h = heuristic(&state);
+            (state, Reverse(h))
+        })
+        .collect::<PriorityQueue<_, _>>();
+
+    let mut state_infos = initial_states
+        .iter()
+        .cloned()
+        .map(|state| (state, C::zero()))
+        .collect::<HashMap<_, _>>();
+
+    while let Some((state, _)) = frontier.pop() {
+        let state_cost = state_infos[&state];
+
+        if is_goal(&state) {
+            return Some(state_cost);
+        }
+
+        for (state_to_succ_cost, successor) in get_successors(&state).into_iter() {
+            let tentative_cost = state_cost + state_to_succ_cost;
+
+            if let Some(succ_cost) = state_infos.get_mut(&successor) {
+                // if tentative cost was too large
+                if *succ_cost <= tentative_cost {
+                    continue;
+                }
+
+                // update the cost stored in the hash map!
+                *succ_cost = tentative_cost;
+
+                // update the priority using the heuristic!
+                frontier
+                    .change_priority(&successor, Reverse(tentative_cost + heuristic(&successor)));
+            } else {
+                state_infos.insert(successor.clone(), tentative_cost);
+
+                let prio = tentative_cost + heuristic(&successor);
+
+                frontier.push(successor, Reverse(prio));
+            }
+        }
+    }
+
+    None
+}
+
+#[allow(dead_code)]
 struct StateInfo<T: Clone + PartialEq + Eq + Hash, C: PrimInt> {
     cost: C,
     parent: Option<T>,
 }
 
 // get the cost and the path taken
-pub fn a_star_search<T, C>(
+#[allow(dead_code)]
+pub fn a_star_search_path<T, C>(
     initial_states: &[T],
 
     // boxed slice of (cost to get to successor, successor)
-    get_successors: impl Fn(&T) -> Box<[(C, T)]>,
+    get_successors: impl Fn(&T) -> Vec<(C, T)>,
 
     is_goal: impl Fn(&T) -> bool,
     heuristic: impl Fn(&T) -> C,
@@ -58,10 +124,10 @@ where
             return Some((state_cost, path));
         }
 
-        for &(state_to_succ_cost, ref successor) in get_successors(&state).iter() {
+        for (state_to_succ_cost, successor) in get_successors(&state).into_iter() {
             let tentative_cost = state_cost + state_to_succ_cost;
 
-            if let Some(succ_info) = state_infos.get_mut(successor) {
+            if let Some(succ_info) = state_infos.get_mut(&successor) {
                 // if tentative cost was too large
                 if succ_info.cost <= tentative_cost {
                     continue;
@@ -72,7 +138,8 @@ where
                 succ_info.parent = Some(state.clone());
 
                 // update the priority using the heuristic!
-                frontier.change_priority(successor, Reverse(tentative_cost + heuristic(successor)));
+                frontier
+                    .change_priority(&successor, Reverse(tentative_cost + heuristic(&successor)));
             } else {
                 state_infos.insert(
                     successor.clone(),
@@ -81,10 +148,10 @@ where
                         parent: Some(state.clone()),
                     },
                 );
-                frontier.push(
-                    successor.clone(),
-                    Reverse(tentative_cost + heuristic(successor)),
-                );
+
+                let prio = tentative_cost + heuristic(&successor);
+
+                frontier.push(successor, Reverse(prio));
             }
         }
     }
