87b8c6f2c12b082d83653a9b252a8f3f34e34b72
flubbydude <stueyrama@gmail.com>
2023-12-21 17:09:51 -0500

    +day21 part 2???

--- advent23_rs/day21/src/main.rs
+++ advent23_rs/day21/src/main.rs
@@ -86,7 +86,7 @@ fn get_successors(grid: &Array2D<Tile>, point: (usize, usize)) -> Vec<(usize, us
     result
 }
 
-fn part1(grid: &Array2D<Tile>, distance: usize) -> HashSet<(usize, usize)> {
+fn get_reachable_gardens(grid: &Array2D<Tile>, distance: usize) -> HashSet<(usize, usize)> {
     let start_pt = find_start(grid);
 
     let mut queue = HashSet::from([start_pt]);
@@ -104,116 +104,6 @@ fn part1(grid: &Array2D<Tile>, distance: usize) -> HashSet<(usize, usize)> {
     queue
 }
 
-fn num_gardens_with(reachable: &HashSet<(usize, usize)>, predicate: fn(usize, usize) -> bool) -> usize {
-    reachable.iter().filter(|&&(i, j)| predicate(i, j)).count()
-}
-
-#[derive(Debug)]
-struct CornerCounts {
-    top_left: usize,
-    top_right: usize,
-    bottom_left: usize,
-    bottom_right: usize,
-}
-
-// corner is where i + j <= 65 basically :O
-fn num_gardens_in_corners_with(
-    reachable: &HashSet<(usize, usize)>,
-    // used for even/odd checking
-    grid_side_length: usize,
-    predicate: fn(usize, usize) -> bool,
-) -> CornerCounts {
-    let half_grid = grid_side_length / 2;
-
-    let mut top_left = 0;
-    let mut top_right = 0;
-    let mut bottom_left = 0;
-    let mut bottom_right = 0;
-
-    let passes_predicate_and_is_garden = |i, j| predicate(i, j) && reachable.contains(&(i, j));
-
-    for i in 0..half_grid + 1 {
-        for j in 0..half_grid - i + 1 {
-            let rev_i = grid_side_length - i - 1;
-            let rev_j = grid_side_length - j - 1;
-            if passes_predicate_and_is_garden(i, j) {
-                top_left += 1;
-            }
-            if passes_predicate_and_is_garden(i, rev_j) {
-                top_right += 1;
-            }
-            if passes_predicate_and_is_garden(rev_i, j) {
-                bottom_left += 1;
-            }
-            if passes_predicate_and_is_garden(rev_i, rev_j) {
-                bottom_right += 1;
-            }
-        }
-    }
-
-    CornerCounts {
-        top_left,
-        top_right,
-        bottom_left,
-        bottom_right,
-    }
-}
-
-fn _part2_old(grid: &Array2D<Tile>, num_steps: usize) -> usize {
-    // assumptions:
-    // num_steps / grid.num_rows() = q + grid.num_rows() / 2
-    // and grid.num_rows() is odd
-    // where q is even
-    // and grid.num_rows() == grid.num_columns()
-
-    assert_eq!(grid.num_rows(), grid.num_columns());
-    assert_eq!(grid.num_rows() % 2, 1);
-
-    let q = num_steps / grid.num_rows();
-
-    assert_eq!(num_steps % grid.num_rows(), grid.num_rows() / 2);
-    assert_eq!(q % 2, 0);
-
-    let reachable_grid = part1(grid, 65);
-
-    let num_even_gardens = num_gardens_with(&reachable_grid, |i, j| (i + j) % 2 == 0);
-    let num_odd_gardens = num_gardens_with(&reachable_grid, |i, j| (i + j) % 2 == 1);
-
-    let CornerCounts {
-        top_left: top_left_even,
-        top_right: top_right_even,
-        bottom_left: bottom_left_even,
-        bottom_right: bottom_right_even,
-    } = num_gardens_in_corners_with(&reachable_grid, |i, j| (i + j) % 2 == 0);
-    let CornerCounts {
-        top_left: top_left_odd,
-        top_right: top_right_odd,
-        bottom_left: bottom_left_odd,
-        bottom_right: bottom_right_odd,
-    } = num_gardens_in_corners_with(&reachable_grid, |i, j| (i + j) % 2 == 1);
-
-    let num_gardens_a = top_left_odd + bottom_right_odd + top_right_even + bottom_left_even;
-    let num_gardens_b = top_left_even + bottom_right_even + top_right_odd + bottom_left_odd;
-    let num_gardens_o =
-        num_even_gardens - top_left_even - top_right_even - bottom_left_even - bottom_right_even;
-    let num_gardens_e =
-        num_odd_gardens - top_left_odd - top_right_odd - bottom_left_odd - bottom_right_odd;
-
-    let o_count = (q + 1) * (q + 1);
-    let e_count = q * q;
-    let a_count = q * (q + 1);
-    let b_count = a_count;
-
-    // error = 0.5038167938931297 * num_steps + -131.7480916030534
-    let error = 0.5038167938931297 * (num_steps as f64) + -131.7480916030534;
-
-    o_count * num_gardens_o
-        + e_count * num_gardens_e
-        + a_count * num_gardens_a
-        + b_count * num_gardens_b
-        - error as usize
-}
-
 fn part2(grid: &Array2D<Tile>, num_steps: usize) -> usize {
     // assumptions:
     // num_steps / grid.num_rows() = q + grid.num_rows() / 2
@@ -221,58 +111,26 @@ fn part2(grid: &Array2D<Tile>, num_steps: usize) -> usize {
     // where q is even
     // and grid.num_rows() == grid.num_columns()
 
-    assert_eq!(grid.num_rows(), grid.num_columns());
-    assert_eq!(grid.num_rows() % 2, 1);
+    let n = grid.num_rows();
 
-    let q = num_steps / grid.num_rows();
+    assert_ne!(n, 0);
+    assert_eq!(n, grid.num_columns());
+    assert_eq!(n % 2, 1);
 
-    assert_eq!(num_steps % grid.num_rows(), grid.num_rows() / 2);
+    let q = num_steps / n;
+
+    assert_eq!(num_steps % n, n / 2);
     assert_eq!(q % 2, 0);
 
-    let reachable_grid = reachable_gardens(grid);
-
-    let num_even_gardens = num_gardens_with(&reachable_grid, |i, j| (i + j) % 2 == 0);
-    let num_odd_gardens = num_gardens_with(&reachable_grid, |i, j| (i + j) % 2 == 1);
-
-    let CornerCounts {
-        top_left: top_left_even,
-        top_right: top_right_even,
-        bottom_left: bottom_left_even,
-        bottom_right: bottom_right_even,
-    } = num_gardens_in_corners_with(&reachable_grid, |i, j| (i + j) % 2 == 0);
-    let CornerCounts {
-        top_left: top_left_odd,
-        top_right: top_right_odd,
-        bottom_left: bottom_left_odd,
-        bottom_right: bottom_right_odd,
-    } = num_gardens_in_corners_with(&reachable_grid, |i, j| (i + j) % 2 == 1);
-
-    let num_gardens_a = top_left_odd + bottom_right_odd + top_right_even + bottom_left_even;
-    let num_gardens_b = top_left_even + bottom_right_even + top_right_odd + bottom_left_odd;
-    let num_gardens_o =
-        num_even_gardens - top_left_even - top_right_even - bottom_left_even - bottom_right_even;
-    let num_gardens_e =
-        num_odd_gardens - top_left_odd - top_right_odd - bottom_left_odd - bottom_right_odd;
-
-    let o_count = (q + 1) * (q + 1);
-    let e_count = q * q;
-    let a_count = q * (q + 1);
-    let b_count = a_count;
-
-    let mut result = o_count * num_gardens_o
-    + e_count * num_gardens_e
-    + a_count * num_gardens_a
-    + b_count * num_gardens_b;
-
-    let part1_grid_temp = Array2D::from_iter_row_major(
+    let grid_times_5_depth = Array2D::from_iter_row_major(
         grid.elements_row_major_iter().cycle().cloned(),
         5 * grid.num_rows(),
         grid.num_columns(),
     )
     .unwrap();
 
-    let part1_grid = Array2D::from_iter_column_major(
-        part1_grid_temp
+    let grid_times_5 = Array2D::from_iter_column_major(
+        grid_times_5_depth
             .elements_column_major_iter()
             .cycle()
             .cloned(),
@@ -281,10 +139,72 @@ fn part2(grid: &Array2D<Tile>, num_steps: usize) -> usize {
     )
     .unwrap();
 
-    let a_top_right_count = q;
-    let b_top
+    let reachable_on_times_5 = get_reachable_gardens(&grid_times_5, 5 * n / 2);
 
-    result
+    let full_square = (0..n)
+        .flat_map(|i| (0..n).map(move |j| (i, j)))
+        .collect::<Box<_>>();
+
+    let shift_and_get_num_squares = |row_shift, column_shift| {
+        full_square
+            .iter()
+            .map(|&(i, j)| (i + row_shift * n, j + column_shift * n))
+            .filter(|point| reachable_on_times_5.contains(point))
+            .count()
+    };
+
+    let o = shift_and_get_num_squares(2, 2);
+    let e = shift_and_get_num_squares(1, 2);
+
+    // dbg!(o);
+    // dbg!(e);
+
+    let a = shift_and_get_num_squares(0, 2);
+    let b = shift_and_get_num_squares(2, 4);
+    let c = shift_and_get_num_squares(4, 2);
+    let d = shift_and_get_num_squares(2, 0);
+
+    // dbg!(a);
+    // dbg!(b);
+    // dbg!(c);
+    // dbg!(d);
+
+    let x = shift_and_get_num_squares(1, 3);
+    let y = shift_and_get_num_squares(3, 3);
+    let z = shift_and_get_num_squares(1, 3);
+    let w = shift_and_get_num_squares(1, 1);
+
+    // dbg!(x);
+    // dbg!(y);
+    // dbg!(z);
+    // dbg!(w);
+
+    let x_prime = shift_and_get_num_squares(0, 3);
+    let y_prime = shift_and_get_num_squares(3, 4);
+    let z_prime = shift_and_get_num_squares(4, 1);
+    let w_prime = shift_and_get_num_squares(1, 0);
+
+    // dbg!(x_prime);
+    // dbg!(y_prime);
+    // dbg!(z_prime);
+    // dbg!(w_prime);
+
+    let o_count = (q - 1) * (q - 1);
+    let e_count = q * q;
+    let x_count = q - 1;
+    let x_prime_count = q;
+
+    // no idea where my error coming from but im missing 15 things every time somehow :)
+    let error = 15 - 15 * q as isize;
+
+    a + b
+        + c
+        + d
+        + o * o_count
+        + e * e_count
+        + (x + y + z + w) * x_count
+        + (x_prime + y_prime + z_prime + w_prime) * x_prime_count
+        - error as usize
 }
 
 fn main() {
@@ -292,9 +212,9 @@ fn main() {
 
     let grid = parse_input(&file_contents);
 
-    println!("{}", part1(&grid, 64).len());
+    println!("{}", get_reachable_gardens(&grid, 64).len());
 
-    // println!("{}", part2(&grid, 26501365));
+    println!("{}", part2(&grid, 26501365));
 
     for q in (0..).step_by(2) {
         let num_steps = q * grid.num_columns() + grid.num_columns() / 2;
@@ -316,7 +236,7 @@ fn main() {
         )
         .unwrap();
 
-        let real = part1(&part1_grid, num_steps);
+        let real = get_reachable_gardens(&part1_grid, num_steps).len();
         let guess = part2(&grid, num_steps);
 
         println!(
@@ -346,6 +266,6 @@ mod tests {
     fn test_part1() {
         let grid = parse_input(TEST_INPUT);
 
-        println!("{}", part1(&grid, 64));
+        assert_eq!(get_reachable_gardens(&grid, 6).len(), 16);
     }
 }
